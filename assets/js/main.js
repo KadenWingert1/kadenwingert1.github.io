e192aac8d217d37b954209cea14b7afc686f4682
Kaden Wingert <kaden.wingert@gmail.com>
2024-08-25 20:53:53 -0500

    Added scroll bar for progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,64 @@
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $nav = $("#nav");
 
-(function($) {
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["361px", "736px"],
+    xsmall: [null, "360px"],
+  });
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ '361px',   '736px'  ],
-			xsmall:  [ null,      '360px'  ]
-		});
+  // Dropdowns.
+  $("#nav > ul").dropotron({
+    mode: "fade",
+    noOpenerFade: true,
+    speed: 300,
+    alignment: "center",
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Scrolly
+  $(".scrolly").scrolly({
+    speed: 1000,
+    offset: function () {
+      return $nav.height() - 5;
+    },
+  });
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			speed: 300,
-			alignment: 'center'
-		});
+  // Nav.
 
-	// Scrolly
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height() - 5; }
-		});
-
-	// Nav.
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
+  // Title Bar.
+  $(
+    '<div id="titleBar">' +
+      '<a href="#navPanel" class="toggle"></a>' +
+      '<span class="title">' +
+      $("#logo").html() +
+      "</span>" +
+      "</div>"
+  ).appendTo($body);
 })(jQuery);
+
+window.addEventListener("scroll", function () {
+  const scrollValue = window.scrollY;
+  const maxScrollValue =
+    document.documentElement.scrollHeight - window.innerHeight;
+
+  // Ensure the scrollValue doesn't exceed maxScrollValue
+  const limitedScrollValue = Math.min(scrollValue, maxScrollValue);
+
+  // Calculate the scroll progress percentage
+  const scrollPercentage = (limitedScrollValue / maxScrollValue) * 100;
+
+  // Update the width of the progress bar
+  document.querySelector(".progress-bar").style.width = `${scrollPercentage}%`;
+});
